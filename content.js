10ed3ea975c4b0975804f0876584790f42bf2031
Alexey Karakulov <ankarakulov@gmail.com>
2016-06-17 02:16:58 +0300

    Removed semicolons.

--- content.js
+++ content.js
@@ -1,12 +1,12 @@
-var LookListenRead = (function() {
+const LookListenRead = (function() {
 
   if (!Array.prototype.last){
     Array.prototype.last = function(){
-      return this[this.length - 1];
-    };
-  };
+      return this[this.length - 1]
+    }
+  }
 
-  const info = s => console.log("LookListenRead: " + s);
+  const info = s => console.log("LookListenRead: " + s)
 
   const commands = {
     next: () => gotoChunk(chunkIx + 1) && reset(),
@@ -17,34 +17,33 @@ var LookListenRead = (function() {
     slowdown: () => speedup(-10),
     speedup: () => speedup(10),
     exitMode: exitMode
-  };
+  }
 
   let playing = false,
       chunkIx = null,
       blockIx = null,
-      chunks = [],
-      blocks = [],
-      options, voice;
+      options, voice
+  const chunks = [], blocks = []
   
   function initVoice(callback) {
-    const voices = speechSynthesis.getVoices();
-    info("Found voices: " + voices.length);
-    voice = voices.find(v => v.name == options.voice);
+    const voices = speechSynthesis.getVoices()
+    info("Found voices: " + voices.length)
+    voice = voices.find(v => v.name == options.voice)
     if (voice) {
-      callback();
+      callback()
     } else {
-      info("Selected voice not found. Waiting for voices...");
-      speechSynthesis.onvoiceschanged = () => initVoice(callback);
+      info("Selected voice not found. Waiting for voices...")
+      speechSynthesis.onvoiceschanged = () => initVoice(callback)
     }
   }
 
   function speakText(text, callback) {
-    const msg = new SpeechSynthesisUtterance(text);
-    msg.rate = options.rate;
-    msg.voice = voice;
-    msg.onend = e => playing && callback();
-    msg.onerror = console.log;
-    speechSynthesis.speak(msg);
+    const msg = new SpeechSynthesisUtterance(text)
+    msg.rate = options.rate
+    msg.voice = voice
+    msg.onend = e => playing && callback()
+    msg.onerror = console.log
+    speechSynthesis.speak(msg)
   }
 
   /* Chunk is a set of blast nodes sharing common block-style parent. Nodes in chunk are
@@ -52,133 +51,136 @@ var LookListenRead = (function() {
   function initChunks() {
 
     function displayType(elem) {
-      return (elem.currentStyle || window.getComputedStyle(elem, "")).display;
+      return (elem.currentStyle || window.getComputedStyle(elem, "")).display
     }
 
     function closestBlock(elem) {
       while (displayType(elem) !== 'block' && elem.parentNode) { elem = elem.parentNode }
-      return elem;
+      return elem
     }
 
-    document.normalize();
-    $("body").blast({ delimiter: options.delimiter, customClass: "looklistenread" });
-    const regexFilter = new RegExp(options.regexFilter);
-    const regexIgnore = new RegExp(options.regexIgnore);
+    document.normalize()
+    $("body").blast({ delimiter: options.delimiter, customClass: "looklistenread" })
+    const regexFilter = new RegExp(options.regexFilter)
+    const regexIgnore = new RegExp(options.regexIgnore)
     Array.from(document.getElementsByClassName("looklistenread"))
       .filter(span => regexFilter.test(span.innerText) && !regexIgnore.test(span.innerText))
          .forEach(span => {
-           const chunk = chunks.length > 0 ? chunks.last() : null;
-           const block = closestBlock(span);
+           const chunk = chunks.length > 0 ? chunks.last() : null
+           const block = closestBlock(span)
            if (chunk && chunk.block === block &&
                chunk.text.length + span.innerText.length <= options.maxLength
            ) {
-             chunk.nodes.push(span);
-             chunk.text += ' ' + span.innerText;
+             chunk.nodes.push(span)
+             chunk.text += ' ' + span.innerText
            } else {
-             chunks.push({nodes:[span], text:span.innerText, block: block});
-             const i = chunks.length - 1;
+             chunks.push({nodes:[span], text:span.innerText, block: block})
+             const i = chunks.length - 1
              blocks.length > 0 && blocks.last().node == block ?
                      blocks.last().chunkIxs.push(i) :
-                     blocks.push({node: block, chunkIxs: [i]});
+                     blocks.push({node: block, chunkIxs: [i]})
            }
-         });
+         })
   }
   
   function bringToRange(i, xs) {
-    return i == null ? i : Math.max(0, Math.min(xs.length - 1, i));
+    return i == null ? i : Math.max(0, Math.min(xs.length - 1, i))
   }
 
   function play() {
-    chunkIx != null || gotoChunk(0);
-    playing = true;
+    chunkIx != null || gotoChunk(0)
+    playing = true
     speakText(chunks[chunkIx].text, () =>
-      chunkIx < chunks.length - 1 ? gotoChunk(chunkIx + 1) && play() : pause());
+      chunkIx < chunks.length - 1 ? gotoChunk(chunkIx + 1) && play() : pause())
   }
   
   function pause() {
-    playing = false;
-    speechSynthesis.cancel();
+    playing = false
+    speechSynthesis.cancel()
   }
 
   function pauseOrResume() {
-    playing ? pause() : play();
+    playing ? pause() : play()
   }
 
   function reset(startPlaying) {
     if (playing) {
-      pause();
-      setTimeout(play, 30);
+      pause()
+      setTimeout(play, 30)
     } else if (startPlaying) {
-      play();
+      play()
     }
   }
 
   function stop() {
-    pause();
-    gotoChunk(null);
+    pause()
+    gotoChunk(null)
   }
 
   function gotoBlock(i) {
-    i = bringToRange(i, blocks);
-    blockIx === i || gotoChunk(blocks[i].chunkIxs[0]) && reset();
+    i = bringToRange(i, blocks)
+    blockIx === i || gotoChunk(blocks[i].chunkIxs[0]) && reset()
   }
 
   // Go to new chunk index, highlight the chunk nodes and return true if new chunk index is
   // not null.
   function gotoChunk(i) {
-    i = bringToRange(i, chunks);
+    i = bringToRange(i, chunks)
     if (chunkIx !== i) {
       if (chunkIx !== null)
-        chunks[chunkIx].nodes.forEach(node => $(node).removeClass("llr-active"));
-      chunkIx = i;
-      blockIx = blocks.findIndex(block => block.chunkIxs.includes(chunkIx));
+        chunks[chunkIx].nodes.forEach(node => $(node).removeClass("llr-active"))
+      chunkIx = i
+      blockIx = blocks.findIndex(block => block.chunkIxs.includes(chunkIx))
       if (chunkIx !== null) {
-        chunks[chunkIx].nodes.forEach(node => $(node).addClass("llr-active"));
-        chunks[chunkIx].nodes[0].scrollIntoViewIfNeeded();
+        chunks[chunkIx].nodes.forEach(node => $(node).addClass("llr-active"))
+        chunks[chunkIx].nodes[0].scrollIntoViewIfNeeded()
       }
     }
-    return chunkIx !== null;
+    return chunkIx !== null
   }
   
   function speedup(percentage) {
-    options.rate *= 1 + 0.01*percentage;
-    reset();
+    options.rate *= 1 + 0.01*percentage
+    reset()
   }
 
   function bindHotkey(hotkey, callback) {
-    Mousetrap.bind(hotkey, e => { callback(); return false });
+    Mousetrap.bind(hotkey, e => {
+      callback()
+      return false
+    })
   }
 
   function enterMode() {
-    initChunks();
+    initChunks()
     initVoice(() => {
-      Mousetrap.unbind(options.hotkeys.enterMode);
-      Object.keys(commands).forEach(cmd => bindHotkey(options.hotkeys[cmd], commands[cmd]));
+      Mousetrap.unbind(options.hotkeys.enterMode)
+      Object.keys(commands).forEach(cmd => bindHotkey(options.hotkeys[cmd], commands[cmd]))
       chunks.forEach((chunk, i) => chunk.nodes.forEach(node => node.ondblclick = e => {
-          gotoChunk(i) && reset(true);
-          e.stopPropagation();
-      }));
+          gotoChunk(i) && reset(true)
+          e.stopPropagation()
+      }))
       blocks.forEach(block => block.node.ondblclick = e => {
-        gotoChunk(block.chunkIxs[0]) && reset(true);
-        e.stopPropagation();
-      });
-      info("Enter speaking mode");
-    });
+        gotoChunk(block.chunkIxs[0]) && reset(true)
+        e.stopPropagation()
+      })
+      info("Enter speaking mode")
+    })
   }
 
   function exitMode() {
-    stop();
-    Object.keys(commands).forEach(cmd => Mousetrap.unbind(options.hotkeys[cmd]));
-    bindHotkey(options.hotkeys.exitMode, exitMode);
-    bindHotkey(options.hotkeys.enterMode, enterMode);
-    info("Exit speaking mode");
+    stop()
+    Object.keys(commands).forEach(cmd => Mousetrap.unbind(options.hotkeys[cmd]))
+    bindHotkey(options.hotkeys.exitMode, exitMode)
+    bindHotkey(options.hotkeys.enterMode, enterMode)
+    info("Exit speaking mode")
   }
 
   return opts => {
-    options = opts;
-    bindHotkey(options.hotkeys.enterMode, enterMode);
+    options = opts
+    bindHotkey(options.hotkeys.enterMode, enterMode)
   }
 
-})();
+})()
 
-chrome.storage.sync.get(defaults, LookListenRead);
+chrome.storage.sync.get(defaults, LookListenRead)
--- options.js
+++ options.js
@@ -1,30 +1,30 @@
-let options;
+let options
 
-const $ = id => document.getElementById(id);
+const $ = id => document.getElementById(id)
 
-const valueOpts = ['rate', 'voice', 'delimiter', 'maxLength', 'regexFilter', 'regexIgnore'];
+const valueOpts = ['rate', 'voice', 'delimiter', 'maxLength', 'regexFilter', 'regexIgnore']
 
 function restore() {
-  console.log("Reading options from sync storage...");
+  console.log("Reading options from sync storage...")
   chrome.storage.sync.get(defaults, items => {
-    options = items;
-    console.log(options);
-    ['sentence', 'element'] .forEach(x => $('delimiter').options.add(new Option(x, x)));
-    initVoiceOptions();
-    valueOpts.forEach(name => $(name).value = options[name]);
-    initHotkeys();
-  });
+    options = items
+    console.log(options)
+    ;['sentence', 'element'].forEach(x => $('delimiter').options.add(new Option(x, x)))
+    initVoiceOptions()
+    valueOpts.forEach(name => $(name).value = options[name])
+    initHotkeys()
+  })
 }
 
 function save() {
-  valueOpts.forEach(name => options[name] = $(name).value);
-  console.log(options);
+  valueOpts.forEach(name => options[name] = $(name).value)
+  console.log(options)
   chrome.storage.sync.set(options, () => {
     // Update status to let user know options were saved.
-    const status = $('status');
-    status.textContent = 'Options saved.';
-    setTimeout((() => status.textContent = ''), 1000);
-  });
+    const status = $('status')
+    status.textContent = 'Options saved.'
+    setTimeout((() => status.textContent = ''), 1000)
+  })
 }
 
 function initVoiceOptions() {
@@ -35,35 +35,35 @@ function initVoiceOptions() {
         voice.name,
         false,
         voice.name == options.voice
-      )));
+      )))
 }
 
 function initHotkeys() {
   Object.keys(options.hotkeys).forEach(cmd =>
-    defaults.hotkeys.hasOwnProperty(cmd) ? addHotkeyInput(cmd) : delete options.hotkeys[cmd]);
+    defaults.hotkeys.hasOwnProperty(cmd) ? addHotkeyInput(cmd) : delete options.hotkeys[cmd])
 }
 
 function addHotkeyInput(cmd) {
-  const input = document.createElement('input');
-  input.id = 'hotkey-' + cmd;
-  input.setAttribute('type', 'button');
-  input.setAttribute('value', options.hotkeys[cmd]);
+  const input = document.createElement('input')
+  input.id = 'hotkey-' + cmd
+  input.setAttribute('type', 'button')
+  input.setAttribute('value', options.hotkeys[cmd])
   input.addEventListener('click', () => {
     Mousetrap.record(sequence => {
       // sequence is an array like ['ctrl+k', 'c']
-      console.log(input.id);
-      input.setAttribute('value', sequence.join(','));
-      options.hotkeys[cmd] = sequence;
-      save();
-    });
-  });
-  const p = document.createElement('p');
-  p.appendChild(document.createTextNode(cmd));
-  p.appendChild(input);
-  $('hotkeys').appendChild(p);
+      console.log(input.id)
+      input.setAttribute('value', sequence.join(','))
+      options.hotkeys[cmd] = sequence
+      save()
+    })
+  })
+  const p = document.createElement('p')
+  p.appendChild(document.createTextNode(cmd))
+  p.appendChild(input)
+  $('hotkeys').appendChild(p)
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  restore();
-  valueOpts.forEach(name => $(name).addEventListener('change', save));
-});
+  restore()
+  valueOpts.forEach(name => $(name).addEventListener('change', save))
+})
